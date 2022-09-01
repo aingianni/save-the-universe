edb1925aa32317039816cc165ead213ca85a584a
aingianni92 <ingianni.anthony@gmail.com>
2022-08-31 21:14:49 -0400

    created class for player and enemy

---
+++ app.js
@@ -0,0 +1,29 @@
+// I need to create two class's. One for the player ship and one for the enemy ship.
+
+class playerShip {
+    constructor() {
+        this.health = 100;
+        this.damage = 10;
+        this.shield = 100;
+    };
+    attack() {
+        // Some code goes here.
+    };
+    repair() {
+        // Some code goes here.
+    };
+};
+
+class enemyShip {
+    constructor() {
+        this.health = 500;
+        this.damage = 25;
+        this.shield = 100;
+    };
+    attack() {
+        // Some code goes here.
+    };
+    repair() {
+        // Some code goes here.
+    };
+};
