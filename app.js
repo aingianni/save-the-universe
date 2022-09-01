a3c733c4a5beb7fc69785d575a92ac8cc509a633
aingianni92 <ingianni.anthony@gmail.com>
2022-08-31 22:47:09 -0400

    wrote pseudo code for render function

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-// I need to create two class's. One for the player ship and one for the enemy ship.
+// All of the cached elements variables.
 
+// Global variables.
+let playerTurn = true;
+
+// I need to create two class's. One for the player ship and one for the enemy ship.
 class playerShip {
     constructor() {
         this.health = 100;
@@ -26,4 +30,9 @@ class enemyShip {
     repair() {
         // Some code goes here.
     };
+};
+
+// Need a render function that will update the page with the stats of each class.
+const render = (evt) => {
+
 };
