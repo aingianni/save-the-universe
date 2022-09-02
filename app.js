1f8570262b6d8cc9e608ad555718acb9a87dc0cc
aingianni92 <ingianni.anthony@gmail.com>
2022-09-02 15:30:13 -0400

    fill in render function and start outline for computer player

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // All of the cached elements variables.
+const playerVitals = document.getElementById('player-vitals');
+const enemyVitals = document.getElementById('enemy-vitals');
 
 // Global variables.
 let playerTurn = true;
 
 // I need to create two class's. One for the player ship and one for the enemy ship.
-class playerShip {
+class PlayerShip {
     constructor() {
         this.health = 100;
         this.damage = 10;
@@ -18,7 +20,7 @@ class playerShip {
     };
 };
 
-class enemyShip {
+class EnemyShip {
     constructor() {
         this.health = 500;
         this.damage = 25;
@@ -32,7 +34,23 @@ class enemyShip {
     };
 };
 
+const player = new PlayerShip();
+const enemy = new EnemyShip();
+
 // Need a render function that will update the page with the stats of each class.
 const render = (evt) => {
+    playerVitals.innerHTML = `
+    Health: ${player.health}
+    Shields: ${player.shield}
+    `;
+
+    enemyVitals.innerHTML = `
+    Health: ${enemy.health}
+    Shields: ${enemy.shield}
+    `;
+};
+
+// Need a function that allows the computer to attack.
+const enemyTurn = (evt) => {
 
 };
